7ab6b59a648cc83b484b40891213d0b9b73cf996
Ali Jetham <alijetham24@gmail.com>
2025-04-26 17:53:17 +0530

    feat: implement view switching in MainHeader,jsx

--- src/renderer/src/components/MainHeader/MainHeader.jsx
+++ src/renderer/src/components/MainHeader/MainHeader.jsx
@@ -1,17 +1,36 @@
 import { useState } from 'react'
 import styles from './MainHeader.module.css'
-import { Settings, SquarePlus, FileType2 } from 'lucide-react'
+import { KanbanSquare, SquarePlus, FileType2 } from 'lucide-react'
 
-export default function MainHeader() {
+export default function MainHeader(props) {
   const [projectName, setProjectName] = useState('projectName')
+
+  function handleViewSwitch() {
+    if (props.view === 'kanban') {
+      props.setView('editor')
+    } else if (props.view === 'editor') {
+      props.setView('kanban')
+    }
+  }
 
   return (
     <div className={styles.mainHeaderContainer}>
       <p className={styles.projectName}>{projectName}</p>
 
-      <div className={styles.actionButtons}>
-        <SquarePlus size={20} />
-        <FileType2 size={20} />
+      <div className={styles.actionButtonsContainer}>
+        <button className={styles.actionButton}>
+          <SquarePlus size={20} />
+        </button>
+
+        {props.view === 'kanban' ? (
+          <button className={styles.actionButton} onClick={handleViewSwitch}>
+            <FileType2 size={20} />
+          </button>
+        ) : (
+          <button className={styles.actionButton} onClick={handleViewSwitch}>
+            <KanbanSquare size={20} />
+          </button>
+        )}
       </div>
     </div>
   )
--- src/renderer/src/components/MainView/MainView.jsx
+++ src/renderer/src/components/MainView/MainView.jsx
@@ -1,15 +1,18 @@
+import { useState } from 'react'
 import styles from './MainView.module.css'
 import MainHeader from '../MainHeader/MainHeader'
 import Kanban from '../Kanban/Kanban'
+import Editor from '../Editor/Editor'
 
 export default function MainView() {
+  const [view, setView] = useState('editor')
+
   return (
     <div className={`${styles.mainViewContainer} noselect`}>
+      <MainHeader view={view} setView={setView} />
 
-      <MainHeader />
-      <Kanban />
-
-
+      {view === 'kanban' ? <Kanban /> : null}
+      {view === 'editor' ? <Editor /> : null}
     </div>
   )
 }
