bfea6ba46d4082e20105ddc0e7e49550424aca93
Ali Jetham <alijetham24@gmail.com>
2025-04-27 17:32:05 +0530

    feat: integrate tiptap in Editor component

--- src/renderer/src/components/Editor/Editor.jsx
+++ src/renderer/src/components/Editor/Editor.jsx
@@ -1,5 +1,19 @@
 import styles from './Editor.module.css'
+import StarterKit from '@tiptap/starter-kit'
+import { EditorProvider } from '@tiptap/react'
+import TaskList from '@tiptap/extension-task-list'
 
 export default function Editor() {
-  return <div className={styles.editorContainer}></div>
+  const extensions = [StarterKit]
+  const content = '<p>Hello World!</p>'
+
+  return (
+    <EditorProvider
+      extensions={extensions}
+      content={content}
+      editable={true}
+      autofocus={true}
+      editorContainerProps={{ className: styles.editorContainer }}
+    ></EditorProvider>
+  )
 }
